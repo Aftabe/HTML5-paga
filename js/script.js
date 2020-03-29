35ecff489d669ae6727ca35e7c75f3ce153e9fb6
Sunny <47844914+Aftabe@users.noreply.github.com>
2020-03-29 11:59:45 +1100

    changes have been applied to these three files

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
 function openCity(evt, cityName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
+  evt.preventDefault();
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
 
-
-  alert('HEY!');
+// Get the element with id="defaultOpen" and click on it
+function preventDefault(e){
+  e.preventDefault();
+  document.getElementById("defaultOpen").click();
+}
