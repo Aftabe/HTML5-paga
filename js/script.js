42be5f10aae3bd3c60d3669f57a5b57fefb5830b
Sunny <47844914+Aftabe@users.noreply.github.com>
2020-07-22 21:10:39 +1000

    new statements added

--- js/script.js
+++ js/script.js
@@ -1,18 +1,29 @@
-function openCity(evt, cityName) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";
+// Variables
+const mailForm = document.getElementById('mail-form'),
+      sendBtn = document.getElementById('send'),
+      resetBtn = document.getElementById('reset');
+      
+
+
+
+
+
+
+
+// Event linstners
+eventListners();
+function eventListners() {
+  // App init
+  document.addEventListener('DOMContentLoaded', appInit)
 }
 
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
 
-// button animated
+
+
+
+// Functions
+
+function appInit() {
+  sendBtn.disabled = true;
+}
+
